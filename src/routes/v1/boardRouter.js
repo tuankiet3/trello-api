21371afa0560dc6c75c0a3ef858c92db6f86c4b9
tuankiet3 <vohtuankiet@dtu.edu.vn>
2024-05-17 15:52:50 +0700

    validation_with_joi

--- src/routes/v1/boardRouter.js
+++ src/routes/v1/boardRouter.js
@@ -1,13 +1,12 @@
 import express from "express";
 import { StatusCodes } from "http-status-codes";
+import { boardValidation } from "~/validations/boardValidation";
 const Router = express.Router();
 
 Router.route("/")
   .get((req, res) => {
     res.status(StatusCodes.OK).json({ message: "api get list board" });
   })
-  .post((req, res) => {
-    res.status(StatusCodes.CREATED).json({ message: "api cread new board" });
-  });
+  .post(boardValidation.createNew);
 
 export const boardRouter = Router;
